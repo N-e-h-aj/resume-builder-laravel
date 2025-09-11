5869f17a86d40082701db1020a7d0bc1dbaef0b6
Neha Jethanandani <nehajethanandani@gmail.com>
2025-09-11 22:15:54 +0530

    index file added

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\resume;
 
 Route::get('/', function () {
-    return view('resume.form');
+    return view('resume.index');
 });
 Route::post('generate',[Resume::class,'generateResume'])->name('resume.generate');
 Route::post('show',[Resume::class,'showResume'])->name('resume.show');
